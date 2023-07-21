fe016706ca5a4c3d87cb7d408fe858d8019c394b
Gaitano <gaitano.george@student.moringaschool.com>
2023-07-21 22:44:31 +0300

    Created Components and Data folders inside src and deleted Unnecessary files

---
+++ src/Components/App.js
@@ -0,0 +1,16 @@
+import React, {useEffect} from "react";
+
+function App (){
+
+    // useEffect(() => {
+    //     fetch('http://localhost:3000/transactions')
+    //     .then(res => res.json())
+    //     .then(data => console.log(data))
+    // })
+
+    // return (
+    //     <h1>Hello</h1>
+    // )
+}
+
+export default App;
